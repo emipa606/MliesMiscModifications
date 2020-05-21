f120bc1d2a6d5d03d7c631dac93259d7c3de6e2c
Mlie <emipa606@users.noreply.github.com>
2020-05-21 23:28:58 +0200

    Removes all siege-types from all loaded factions

---
+++ Source/MliesMiscModifications/Patcher.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace MliesMiscModifications
+{
+    [StaticConstructorOnStartup]
+    class Patcher
+    {
+        static Patcher()
+        {
+            var list = DefDatabase<FactionDef>.AllDefs.ToList();
+            foreach (FactionDef factionDef in list)
+            {
+                factionDef.canSiege = false;
+            }
+        }
+    }
+}
